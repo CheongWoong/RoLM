cd5bc2b1374e276a9858e79b4ee24389bc4806c1
cheongwoong <cw.kang@kaist.ac.kr>
2025-08-21 14:21:30 +0900

    update

--- scripts/experiments/finetuned/inference/reft/CommonsenseQA.sh
+++
@@ -1,8 +0,0 @@
-dataset_name=CommonsenseQA
-model_name=Llama-3.1-8B-Instruct
-
-max_new_tokens=256
-for ps in zero-shot few-shot zero-shot-cot
-do
-    python src/inference_finetuned_reft.py --dataset_name $dataset_name --model_name $model_name --prompting_strategy $ps --max_new_tokens $max_new_tokens --save_logprobs
-done
